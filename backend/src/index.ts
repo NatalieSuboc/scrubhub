7de772314604bfa11fdf257dcc3dbf512ee69d43
Natalie Suboc <nsuboc@berkeley.edu>
2023-06-24 00:07:32 -0700

    Implement backend server connection

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+// NOTE: Doesn't work yet, just some boilerplate JS
+const express = require('express');
+
+const app = express();
+// app.use(cors());
+
+const PORT = 4000; // Runs on PORT 4000
+
+app.listen(PORT, (req: Express.Request, res: Express.Response) => {
+    console.log(`Server started at port ${PORT}`);
+})
