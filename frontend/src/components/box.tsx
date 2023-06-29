62f78ceea8026c50993a0cf86b0cbe6f764e32d7
Jakin Chan <JakinChan200@gmail.com>
2023-06-29 12:45:04 -0700

    Deleted unwanted files and tested components

---
+++ frontend/src/components/box.tsx
@@ -0,0 +1,8 @@
+import React, { useEffect } from "react";
+import "./box.css";
+
+//remove this component entirely after demo
+export const Box: React.FC = () => {
+
+  return <div className="container">{"Hello"}</div>;
+};
