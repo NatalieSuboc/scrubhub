f5509cdac7a452eeb8f79780604d1829c009b15e
Natalie Suboc <nsuboc@berkeley.edu>
2023-07-11 17:25:32 -0700

    Add user signin (auth) functionality

---
+++ backend/src/middleware/auth.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from 'express';
+const jwt = require("jsonwebtoken");
+const auth = require('../middleware/auth');
+
+module.exports = function (req: Request, res: Response, next: NextFunction) {
+    // Requires a token to be put in the header of the api req
+    const token = req.header('token');
+    if (!token) {
+        return res.status(401).json({ message: "authentication error"});
+    }
+    try {
+        // "scrubhub" is a randomly chosen string
+        const verified = jwt.verify(token, "scrubhub");
+        req.body = verified.body;
+        // NextFunction signals the auth's completion
+        next();
+
+    } catch (e: any) {
+        console.log(e);
+        res.status(500).send({ message: "invalid token" });
+    }
+}
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -4,10 +4,14 @@ const { v4: uuidv4 } = require('uuid');
 const bodyParser = require('body-parser');
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
+const auth = require('../middleware/auth');
 const User = require('../models/User');
 const Task = require('../models/Task');
+
+const SECRET = "scrubhub";
 
 const middlewareFunc = function (req: Request, res: Response, next: NextFunction) {
     next();
@@ -44,7 +48,6 @@ router.post("/create",
 
         try {
             // first check if a user already exists
-            // TODO would check the username too once that's set up
             let user = await User.findOne({ username });
             if (user) {
                 return res.status(400).json({ message: "User already exists"});
@@ -53,13 +56,64 @@ router.post("/create",
             user = new User({ userid, username, password, points, email });
             await user.save();
 
-            res.status(201).send({
-                message: "User created",
-                userid: userid,
-            });
+            // Authentication of user - signs the user's token
+            const userPayload = { userid: userid, username: username };
+            jwt.sign(userPayload, SECRET, { expiresIn: 100000 /* Basically never */ },
+                (error: any, token: String) => {
+                    if (error) {
+                        throw error; // Will be caught and return "Error in creating user"
+                    }
+                    res.status(201).send({
+                        message: "User created",
+                        userid: userid,
+                        token: token,
+                    });
+                }
+            );
+
         } catch (e: any) {
             console.log(e.message);
             res.status(500).send("Error in creating user");
+        }
+    }
+);
+
+/**
+ * @method - POST
+ * @param - /signin
+ * @description - authenticates ("signs in") a user
+ */
+router.post("/signin", middlewareFunc, 
+    async (req: Request, res: Response) => {
+
+        try {
+            let user = await User.findOne({ username: req.body.username });
+            if (!user) {
+                return res.status(400).json({ error: "User not found" });
+            }
+            // Here's the hard part
+            // Compares the plaintext password (first parameter) with the encrypted password
+            // from the db (second parameter) - SHOULD return true if the plaintext is correct
+            const matches = await bcrypt.compare(req.body.password, user.password);
+            if (!matches) {
+                return res.status(400).json({ message: "Incorrect password"});
+            }
+            const userPayload = { userid: user.userid, username: user.username };
+            jwt.sign(userPayload, SECRET, { expiresIn: 100000 /* Basically never */ },
+            (error: any, token: String) => {
+                if (error) {
+                    throw error; // Will be caught and return "Internal error in user signin"
+                }
+                res.status(200).send({
+                    message: "User signed in",
+                    userid: user.userid,
+                    token: token,
+                });
+            }
+        );
+        } catch (e: any) {
+            console.log(e.message);
+            res.status(500).send("Internal error in user signin");
         }
     }
 );
@@ -69,7 +123,7 @@ router.post("/create",
  * @param - /get?userid=<userid>
  * @description - retrieves a user based on userid
  */
-router.get("/get", middlewareFunc, 
+router.get("/get", auth, 
     async (req: Request, res: Response) => {
         // Error checking
         if (!req.query.userid) {
@@ -97,7 +151,7 @@ router.get("/get", middlewareFunc,
  * @param - /update?userid=<userid>
  * @description - updates a user based on userid
  */
-router.put("/update", middlewareFunc,
+router.put("/update", auth,
     async (req: Request, res: Response) => {
         // Error checking
         if (!req.query.userid) {
