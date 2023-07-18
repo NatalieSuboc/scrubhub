e17f3039fc0b113975585194822c664484ca8fd9
Natalie Suboc <nsuboc@berkeley.edu>
2023-07-18 00:48:03 -0700

    Set up components for timer

---
+++ frontend/src/components/PTimer.tsx
@@ -0,0 +1,6 @@
+import './PTimer.css';
+
+export const PTimer: React.FC = () => {
+
+    return <div className="container"></div>;
+  };
---
+++ frontend/src/components/Pomodoro.tsx
@@ -0,0 +1,6 @@
+import './Pomodoro.css';
+
+export const Pomodoro: React.FC = () => {
+
+    return <div className="container"></div>;
+  };
